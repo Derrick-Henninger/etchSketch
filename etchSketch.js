9357050bbbdf561fcaab8914db7d7707fa8dfb53
RoyDonq <137445574+RoyDonq@users.noreply.github.com>
2024-06-27 14:49:56 -0700

    Initial commit did not stick for some reason. This is now my first commit as a result. Grid container and divs form a grid with equal size squares via flexbox and loops.

---
+++ etchSketch.js
@@ -0,0 +1,54 @@
+
+
+const body =document.querySelector('body');
+const gridBtn = document.createElement('button');
+gridBtn.textContent = "Grid Size";
+const gridContainer = document.querySelector('#grid16x16');
+body.appendChild(gridBtn);
+body.appendChild(gridContainer);
+
+
+// Add button on top of screen to prompt user for how many squares 
+//per side they would like. The existing grid should be removed/replaced
+//should be able to enter 64 and have a brand new 64x64 grid pop up 
+//without changing the total amount of pixels used. Use flexbox.
+
+
+
+gridBtn.addEventListener("click", () => {
+    let gridSize = prompt("How many squares per side would you like in the grid below?");
+    return gridSize;
+}
+)
+//when ready to run change sqrs to equal gridSize.
+let sqrsPerSide = 50;
+makeGrid (sqrsPerSide);
+
+
+function makeGrid (){
+ for (let i = 0; i < sqrsPerSide; i++){
+
+    let gridColumns = document.createElement('div');
+    gridColumns.classList.add('column');
+    gridContainer.appendChild(gridColumns);
+    
+
+    for (let j = 0; j < sqrsPerSide; j++){
+    let gridRows = document.createElement('div');
+    gridRows.classList.add('rows');
+    gridColumns.appendChild(gridRows);
+    
+    
+    }
+  
+}}
+draw();
+function draw () {
+    let numberOfDivs = document.getElementsByClassName('column');
+
+    numberOfDivs.addEventListener('mouseover', () => {
+        numberOfDivs.classList.add('highlight');    
+    })
+}
+
+
